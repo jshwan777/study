218985196a70cd60edfb28a90029244ce173a013
jsh <jsh@DESKTOP-8FPK662>
2022-04-04 15:18:03 +0900

    test commit

---
+++ src/test/java/com/study/kafkastudy/StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.kafkastudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
